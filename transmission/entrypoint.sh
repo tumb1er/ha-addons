c974556a7d3b2da064e9cb1c688713099ee70de1
tumb1er <zimbler@gmail.com>
2025-08-30 12:40:36 +0300

    fix incomplete volume access

--- transmission/entrypoint.sh
+++ transmission/entrypoint.sh
@@ -3,8 +3,8 @@
 if [ ! -f "/transmission/config/settings.json" ]; then
     transmission-daemon \
       --no-watch-dir \
-      --incomplete-dir /transmission/incomplete/ \
-      --download-dir /transmission/downloads/ \
+      --incomplete-dir /transmission/incomplete \
+      --download-dir /transmission/downloads \
       -d \
       2>/transmission/config/settings.json
 
