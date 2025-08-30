b77889afd8fde17ade6074473523350e52bf21a7
tumb1er <zimbler@gmail.com>
2025-08-30 12:36:50 +0300

    allow access

--- transmission/entrypoint.sh
+++ transmission/entrypoint.sh
@@ -7,6 +7,12 @@ if [ ! -f "/transmission/config/settings.json" ]; then
       --download-dir /transmission/downloads/ \
       -d \
       2>/transmission/config/settings.json
+
+      sed -i \
+        -e 's/"rpc-whitelist-enabled": true/"rpc-whitelist-enabled": false/g' \
+        -e 's/"rpc-host-whitelist-enabled": true/"rpc-host-whitelist-enabled": false/g' \
+        -e 's/"rpc-url": "\/transmission\/"/"rpc-url": "\/"/g' \
+        /transmission/config/settings.json
 fi
 
 exec "$@"
