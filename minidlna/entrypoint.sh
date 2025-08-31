844135de0a3031c56de39853ac1b774302cd34c9
tumb1er <zimbler@gmail.com>
2025-08-31 09:54:24 +0300

    auto ingress port

--- minidlna/entrypoint.sh
+++ minidlna/entrypoint.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/usr/bin/with-contenv bashio
 
-if [ ! -f "/config/minidlna.conf" ]; then
-    cat /etc/minidlna.conf | sed \
-      -e 's/#friendly_name=My DLNA Server/friendly_name=HA/g' \
-      -e 's/#enable_subtitles=yes/enable_subtitles=no/g' \
-      -e 's/#db_dir=\/var\/cache\/minidlna/db_dir=\/config\/minidlna/g' \
-      -e 's/notify_interval=900/notify_interval=60/g' \
-      > /config/minidlna.conf
-fi
+declare ingress_port
+ingress_port=$(bashio::addon.ingress_port)
+
+cat /etc/minidlna.conf | sed \
+  -e 's/#friendly_name=My DLNA Server/friendly_name=HA/g' \
+  -e 's/#enable_subtitles=yes/enable_subtitles=no/g' \
+  -e 's/#db_dir=\/var\/cache\/minidlna/db_dir=\/config\/minidlna/g' \
+  -e 's/notify_interval=900/notify_interval=60/g' \
+  -e "s/port=8200/port=%{ingress_port}/g" \
+  > /config/minidlna.conf
 
 exec "$@"
