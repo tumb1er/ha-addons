5d0353786b85a02783634e534be46b62f5bf8e59
tumb1er <zimbler@gmail.com>
2025-08-31 09:22:45 +0300

    lower minidlna notify interval

--- minidlna/entrypoint.sh
+++ minidlna/entrypoint.sh
@@ -5,6 +5,7 @@ if [ ! -f "/config/minidlna.conf" ]; then
       -e 's/#friendly_name=My DLNA Server/friendly_name=HA/g' \
       -e 's/#enable_subtitles=yes/enable_subtitles=no/g' \
       -e 's/#db_dir=\/var\/cache\/minidlna/db_dir=\/config\/minidlna/g' \
+      -e 's/notify_interval=900/notify_interval=60/g' \
       > /config/minidlna.conf
 fi
 
